e4ee914ff620ab5d0468bba2903b255e2cc86cc5
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-04-07 12:33:20 -0500

    Se agregan los archivos para práctica de clases dinámicas con VueJS y Bootstrap 4

---
+++ clases.js
@@ -0,0 +1,13 @@
+const app = new Vue(
+   {
+      el: "#app",
+      data: {
+         clase_fondo : "bg-warning",
+         clase_color: false
+      },
+      methods: {
+      },
+      computed: {
+      }
+   }
+);
