978e6b2c8d0744694e00fe99c877d226dace3c65
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-04-11 06:22:01 -0500

    Error intencional para demostrar la comunicacion entre componentes - Proxima implementacion de Vuex solucionará esta issue

--- componentes/hijo.js
+++ componentes/hijo.js
@@ -4,8 +4,21 @@ Vue.component(
       template: `
          <div class="py-5 px-5 bg-success">
             <h4>Componente hijo: {{numero}}</h4>
+            <h4>Nombre: {{nombre}}</h4>
+            <button class="btn btn-warning" v-on:click="numero++"> + </button>
          </div>
       `,
-      props: ["numero"]
+      props: ["numero"],
+      data() {
+         return {
+            nombre: "Hugo Alberto Rivera Díaz"
+         }
+      },
+      mounted() {
+         this.$emit(
+            "nombreHijo",
+            this.nombre
+         )
+      }
    }
 )
--- componentes/padre.js
+++ componentes/padre.js
@@ -4,13 +4,15 @@ Vue.component(
       template: `
          <div class="p-5 bg-dark text-white">
             <h2>Componente Padre: {{numeroPadre}} </h2>
-            <hijo class="my-3" :numero="numeroPadre"></hijo>
+            <h6>Nombre recibido desde el Hijo: {{nombrePadre}} </h6>
+            <hijo class="my-3" :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
             <button class="btn btn-info" v-on:click="numeroPadre++"> + </button>
          </div>
       `,
       data() {
          return {
-            numeroPadre: 0
+            numeroPadre: 0,
+            nombrePadre: ""
          }
       }
    }
