09331e4f6863863e95627d64a5b114d3f49c5e3e
Hug0Albert0 <hugorivera.cool.player@gmail.com>
2020-04-11 13:51:22 -0500

    Consumo de api local, uso y mapeo de mutaciones y acciones

--- js/vuex.js
+++ js/vuex.js
@@ -6,13 +6,18 @@ Vue.component(
             <button class="btn btn-info btn-lg" @click="aumentar(2)">Aumentar</button>
             <button class="btn btn-danger btn-lg" @click="disminuir(2)">Disminuir</button>
             <h1>Numero Hijo: {{numero}} </h1>
+            <button class="btn btn-dark btn-lg my-4" @click="obtenerCursos">Obtener Cursos</button>
+            <ul v-for="curso of cursos">
+               <li>{{curso.nombre}}</li>
+            </ul>
          </div>
       `,
       computed: {
-         ...Vuex.mapState(["numero"])
+         ...Vuex.mapState(["numero", "cursos"])
       },
       methods: {
-         ...Vuex.mapMutations(["aumentar", "disminuir"])
+         ...Vuex.mapMutations(["aumentar", "disminuir"]),
+         ...Vuex.mapActions(["obtenerCursos"])
       }
    },
 )
@@ -32,6 +37,45 @@ Vue.component(
    }
 )
 
+const store = new Vuex.Store({
+   state: {
+      numero: 10,
+      cursos: []
+   },
+   mutations: {
+      aumentar(state, aumento) {
+         if (state.numero < 100) {
+            state.numero += aumento;
+         }
+      },
+      disminuir(state, decremento) {
+         if(state.numero > 0) {
+            state.numero -= decremento;
+         }
+      },
+      rellenarCursos (state, cursosAccion) {
+         state.cursos = cursosAccion;
+      }
+   },
+   actions: {
+      obtenerCursos: async function ({commit}) {
+         const data = await fetch("../src/cursos.json");
+         const cursos = await data.json();
+         commit("rellenarCursos", cursos);
+      }
+   }
+})
+
+const app = new Vue(
+   {
+      el: "#app",
+      store,
+      data: {
+         saludo : "Probando componentes de Vue JS"
+      }
+   }
+);
+
 /*
 Vue.component(
    "mensaje",
@@ -47,33 +91,3 @@ Vue.component(
       }
    },
 )*/
-
-const store = new Vuex.Store({
-   state: {
-      numero: 10
-   },
-   mutations: {
-      aumentar(state, aumento) {
-         if (state.numero < 100) {
-            state.numero += aumento;
-         }
-      },
-      disminuir(state, decremento) {
-         if(state.numero > 0) {
-            state.numero -= decremento;
-         }
-      }
-   }
-})
-
-const app = new Vue(
-   {
-      el: "#app",
-      store,
-      data: {
-         saludo : "Probando componentes de Vue JS"
-      }
-   }
-);
-
-
